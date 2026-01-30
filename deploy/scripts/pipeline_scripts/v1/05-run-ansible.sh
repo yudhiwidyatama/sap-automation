0bda6eec7214b32dc9c9aab7d9452c0ce0bd9bb8
Yudhi Widyatama <yudhi.widyatama@telkom.co.id>
2026-01-30 08:48:35 +0000

    Updated 05-run-ansible.sh

--- deploy/scripts/pipeline_scripts/v1/05-run-ansible.sh
+++ deploy/scripts/pipeline_scripts/v1/05-run-ansible.sh
@@ -172,8 +172,6 @@ fi
 filename=./config/Ansible/"${filename_without_prefix}"_pre.yml
 
 echo "Check if file: ${filename} exists"
-echo "$PWD/${filename}"
-ls -l "${filename}"
 
 if [ -f "${filename}" ]; then
 	echo "##[group]- preconfiguration"
